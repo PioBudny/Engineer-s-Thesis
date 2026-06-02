693d36d5c2d629b270af1e11f6ef79fe8c5e6d45
PioBudny <PioBudny@gmail.com>
2026-06-02 18:59:35 +0200

    Testy1

--- TDR_software/src/Main.c
+++ TDR_software/src/Main.c
@@ -144,7 +144,7 @@ int main()
                         break;
 
                     case CMD_DEBUG_LED_ON: {
-                        //pll_load_tcs(I2C_PORT);
+                        pll_load_tcs(I2C_PORT);
 
                         printf("OK\n");
                         gpio_put(LED_PIN, 1);
